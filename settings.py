23dc0df66a034eb6af8a6d06e5ae2e3c65aa60a2
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2024-06-13 13:20:34 +0200

    highlight sentences using cue words

--- settings.py
+++ settings.py
@@ -1,12 +1,37 @@
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import PydanticBaseSettingsSource, TomlConfigSettingsSource
+from pydantic import Field
+from typing import Type, Tuple
 
 
 class Settings(BaseSettings):
-    PORT: int
-    SVM_BASELINE_PATH: str
-    COEF_MAX_TOP_K: int
-    model_config = SettingsConfigDict(env_file="settings.toml")
+    # Interface
+    PORT: int = Field(default=5050, description="Field to run the app.")
+    # Detection system
+    MODEL_TYPE: str = Field(description="Detection model type")
+    SVM_BASELINE_PATH: str = Field(description="Path to trained SVM model")
+    SVM_BASELINE__MAX_TOP_K: int = Field(description="whatever")
+    # Visualization
+    USE_CUE_WORDS : bool = Field(description="Whether to highlight based on cue words")
+    CUE_PERCENTILE_CUTOFF : float = Field(
+        description="A float in the (0, 1) range defining the percentile cutoff for cue words.")
+    # Processing
+    BATCH_SIZE: int = Field(description="Number of documents in batch")
+    LANGUAGES: list[str] = Field(description="Languages to load detectors for.")
+
+    model_config = SettingsConfigDict(toml_file="settings.toml")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (TomlConfigSettingsSource(settings_cls),)
 
 
 settings = Settings()
